1d2e82a1f701a39deb45b991659b9118832e2a79
Junyang HE <nathanhejunyang@gmail.com>
2018-04-19 22:31:11 +0200

    Improve avatar quality, again

--- front/src/providers/avatar/avatar.ts
+++ front/src/providers/avatar/avatar.ts
@@ -23,7 +23,7 @@ export class AvatarProvider {
         this.idToLeaf(idLeaf),
     )
       .subscribe(
-        function handleValue(values) {
+        (values) => {
           let rabbit: Blob = values[0];
           let leaf: Blob = values[1];
 
@@ -33,8 +33,9 @@ export class AvatarProvider {
 
           var canvasWidth = 0;
           var canvasHeight = 0;
+          var downScaleRatio = 1;
 
-          rabbitImg.onload = function() {
+          rabbitImg.onload = () => {
             // Make it visually fill the positioned parent
             canvas.style.width = '100%';
             canvas.style.height = '100%';
@@ -53,19 +54,9 @@ export class AvatarProvider {
             canvasWidth = x1;
             canvasHeight = y1;
 
-            while (x2 > 2 * x1 && y2 > 2 * y1) {
-              var oc = document.createElement('canvas');
-              var octx = oc.getContext('2d');
+            downScaleRatio = x1 / x2 > y1 / y2 ? x1 / x2 : y1 / y2;
+            rabbitImg = this.downScaleImage(rabbitImg, downScaleRatio);
 
-              oc.width = x2 * 0.5;
-              oc.height = y2 * 0.5;
-
-              octx.drawImage(rabbitImg, 0, 0, oc.width, oc.height);
-
-              x2 = oc.width
-              y2 = oc.height
-              rabbitImg = oc
-            }
 
             const posX = (x1 * y2 - x2 * y1) / 2 / y2;
             const posY = 0;
@@ -75,25 +66,13 @@ export class AvatarProvider {
             leafImg.src = URL.createObjectURL(leaf);
           }
 
-          leafImg.onload = function() {
+          leafImg.onload = () => {
             const x1 = canvasWidth;
             const y1 = canvasHeight;
             let x2 = leafImg.width;
             let y2 = leafImg.height;
 
-            while (x2 > 2 * x1 && y2 > 2 * y1) {
-              var oc = document.createElement('canvas');
-              var octx = oc.getContext('2d');
-
-              oc.width = x2 * 0.5;
-              oc.height = y2 * 0.5;
-
-              octx.drawImage(leafImg, 0, 0, oc.width, oc.height);
-
-              x2 = oc.width
-              y2 = oc.height
-              leafImg = oc
-            }
+            leafImg = this.downScaleImage(leafImg, downScaleRatio);
 
             const drawHeight = y1 / 2.3;
             const drawWitdh = drawHeight * x2 / y2;
@@ -119,4 +98,134 @@ export class AvatarProvider {
       .get('../../assets/imgs/avatar/leaf_' + id + '.png', { responseType: 'blob' });
   }
 
+
+  // ref: https://stackoverflow.com/questions/18922880/html5-canvas-resize-downscale-image-high-quality
+  // ref: http://jsfiddle.net/gamealchemist/kpQyE/14/
+  // author: gamealchemist
+  // scales the image by (float) scale < 1
+  // returns a canvas containing the scaled image.
+  private downScaleImage(img, scale): HTMLCanvasElement {
+    var imgCV = document.createElement('canvas');
+    imgCV.width = img.width;
+    imgCV.height = img.height;
+    var imgCtx = imgCV.getContext('2d');
+    imgCtx.drawImage(img, 0, 0);
+    return this.downScaleCanvas(imgCV, scale);
+  }
+
+  // scales the canvas by (float) scale < 1
+  // returns a new canvas containing the scaled image.
+  private downScaleCanvas(cv, scale): HTMLCanvasElement {
+    if (!(scale < 1) || !(scale > 0)) throw ('scale must be a positive number <1 ');
+    scale = normaliseScale(scale);
+    var sqScale = scale * scale; // square scale =  area of a source pixel within target
+    var sw = cv.width; // source image width
+    var sh = cv.height; // source image height
+    var tw = Math.floor(sw * scale); // target image width
+    var th = Math.floor(sh * scale); // target image height
+    var sx = 0, sy = 0, sIndex = 0; // source x,y, index within source array
+    var tx = 0, ty = 0, yIndex = 0, tIndex = 0; // target x,y, x,y index within target array
+    var tX = 0, tY = 0; // rounded tx, ty
+    var w = 0, nw = 0, wx = 0, nwx = 0, wy = 0, nwy = 0; // weight / next weight x / y
+    // weight is weight of current source point within target.
+    // next weight is weight of current source point within next target's point.
+    var crossX = false; // does scaled px cross its current px right border ?
+    var crossY = false; // does scaled px cross its current px bottom border ?
+    var sBuffer = cv.getContext('2d').
+      getImageData(0, 0, sw, sh).data; // source buffer 8 bit rgba
+    var tBuffer = new Float32Array(3 * tw * th); // target buffer Float32 rgb
+    var sR = 0, sG = 0, sB = 0; // source's current point r,g,b
+
+    for (sy = 0; sy < sh; sy++) {
+      ty = sy * scale; // y src position within target
+      tY = 0 | ty;     // rounded : target pixel's y
+      yIndex = 3 * tY * tw;  // line index within target array
+      crossY = (tY !== (0 | (ty + scale)));
+      if (crossY) { // if pixel is crossing botton target pixel
+        wy = (tY + 1 - ty); // weight of point within target pixel
+        nwy = (ty + scale - tY - 1); // ... within y+1 target pixel
+      }
+      for (sx = 0; sx < sw; sx++ , sIndex += 4) {
+        tx = sx * scale; // x src position within target
+        tX = 0 | tx;    // rounded : target pixel's x
+        tIndex = yIndex + tX * 3; // target pixel index within target array
+        crossX = (tX !== (0 | (tx + scale)));
+        if (crossX) { // if pixel is crossing target pixel's right
+          wx = (tX + 1 - tx); // weight of point within target pixel
+          nwx = (tx + scale - tX - 1); // ... within x+1 target pixel
+        }
+        sR = sBuffer[sIndex];   // retrieving r,g,b for curr src px.
+        sG = sBuffer[sIndex + 1];
+        sB = sBuffer[sIndex + 2];
+        if (!crossX && !crossY) { // pixel does not cross
+          // just add components weighted by squared scale.
+          tBuffer[tIndex] += sR * sqScale;
+          tBuffer[tIndex + 1] += sG * sqScale;
+          tBuffer[tIndex + 2] += sB * sqScale;
+        } else if (crossX && !crossY) { // cross on X only
+          w = wx * scale;
+          // add weighted component for current px
+          tBuffer[tIndex] += sR * w;
+          tBuffer[tIndex + 1] += sG * w;
+          tBuffer[tIndex + 2] += sB * w;
+          // add weighted component for next (tX+1) px
+          nw = nwx * scale
+          tBuffer[tIndex + 3] += sR * nw;
+          tBuffer[tIndex + 4] += sG * nw;
+          tBuffer[tIndex + 5] += sB * nw;
+        } else if (!crossX && crossY) { // cross on Y only
+          w = wy * scale;
+          // add weighted component for current px
+          tBuffer[tIndex] += sR * w;
+          tBuffer[tIndex + 1] += sG * w;
+          tBuffer[tIndex + 2] += sB * w;
+          // add weighted component for next (tY+1) px
+          nw = nwy * scale
+          tBuffer[tIndex + 3 * tw] += sR * nw;
+          tBuffer[tIndex + 3 * tw + 1] += sG * nw;
+          tBuffer[tIndex + 3 * tw + 2] += sB * nw;
+        } else { // crosses both x and y : four target points involved
+          // add weighted component for current px
+          w = wx * wy;
+          tBuffer[tIndex] += sR * w;
+          tBuffer[tIndex + 1] += sG * w;
+          tBuffer[tIndex + 2] += sB * w;
+          // for tX + 1; tY px
+          nw = nwx * wy;
+          tBuffer[tIndex + 3] += sR * nw;
+          tBuffer[tIndex + 4] += sG * nw;
+          tBuffer[tIndex + 5] += sB * nw;
+          // for tX ; tY + 1 px
+          nw = wx * nwy;
+          tBuffer[tIndex + 3 * tw] += sR * nw;
+          tBuffer[tIndex + 3 * tw + 1] += sG * nw;
+          tBuffer[tIndex + 3 * tw + 2] += sB * nw;
+          // for tX + 1 ; tY +1 px
+          nw = nwx * nwy;
+          tBuffer[tIndex + 3 * tw + 3] += sR * nw;
+          tBuffer[tIndex + 3 * tw + 4] += sG * nw;
+          tBuffer[tIndex + 3 * tw + 5] += sB * nw;
+        }
+      } // end for sx
+    } // end for sy
+
+    // create result canvas
+    var resCV = document.createElement('canvas');
+    resCV.width = tw;
+    resCV.height = th;
+    var resCtx = resCV.getContext('2d');
+    var imgRes = resCtx.getImageData(0, 0, tw, th);
+    var tByteBuffer = imgRes.data;
+    // convert float32 array into a UInt8Clamped Array
+    var pxIndex = 0; //
+    for (sIndex = 0, tIndex = 0; pxIndex < tw * th; sIndex += 3, tIndex += 4, pxIndex++) {
+      tByteBuffer[tIndex] = 0 | (tBuffer[sIndex]);
+      tByteBuffer[tIndex + 1] = 0 | (tBuffer[sIndex + 1]);
+      tByteBuffer[tIndex + 2] = 0 | (tBuffer[sIndex + 2]);
+      tByteBuffer[tIndex + 3] = 255;
+    }
+    // writing result to canvas.
+    resCtx.putImageData(imgRes, 0, 0);
+    return resCV;
+  }
 }
