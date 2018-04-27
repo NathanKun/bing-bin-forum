0841f1cd2b962933fe544fda9f3303c4374b26fb
HE Junyang <nathanhejunyang@gmail.com>
2018-04-27 15:12:32 +0200

    Update Popover component, add delete thread feature

--- front/src/components/popover/popover.ts
+++ front/src/components/popover/popover.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+
+import { ThreadProvider } from '../../providers/thread/thread';
 
 /**
  * Generated class for the PopoverComponent component.
@@ -11,6 +14,54 @@ import { Component } from '@angular/core';
   templateUrl: 'popover.html'
 })
 export class PopoverComponent {
-  items: any;
-  text: string;
+  threadId: number;
+  private threads: [any];
+
+  constructor(public viewCtrl: ViewController, public navParams: NavParams,
+    private threadProvider: ThreadProvider, public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+    this.threadId = navParams.get('threadId');
+    this.threads = navParams.get('threads');
+  }
+
+  close() {
+    this.viewCtrl.dismiss();
+  }
+
+  share(event) {
+    console.log("share on click");
+    console.log(event);
+    console.log(this.threadId);
+    this.close();
+  }
+
+  delete(event) {
+    console.log("delete on click");
+    console.log(event);
+    console.log(this.threadId);
+    this.close();
+
+    let loading = this.loadingCtrl.create();
+    loading.present();
+
+    this.threadProvider.deleteThread(this.threadId).subscribe((res) => {
+      if (res.valid) {
+        this.threads.forEach((t, index) => {
+          if(t.id === this.threadId) {
+            this.threads.splice(index, 1);
+          }
+        });
+
+        loading.dismiss();
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Oops',
+          subTitle: res.error ? res.error : 'Erreur',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    })
+
+  }
 }
